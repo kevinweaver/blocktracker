562da60a3a9c22755def78834613c6186f817e03
Kevin Weaver <kevinweaver2@gmail.com>
2021-07-26 11:57:54 -0400

    Remove dist folder

--- dist/App.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-require("dotenv").config();
-var Web3 = require("web3");
-var web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-console.log(process.env.INFURA_RINKEBY_ENDPOINT);
-web3.eth.getAccounts().then(console.log);
-//# sourceMappingURL=App.js.map
