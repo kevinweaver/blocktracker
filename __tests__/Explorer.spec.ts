8615e28ebfd8324142b388e06f325325da0940b5
Kevin Weaver <kevinweaver2@gmail.com>
2021-08-10 14:40:28 -0400

    Move tests folder

---
+++ __tests__/Explorer.spec.ts
@@ -0,0 +1,51 @@
+import Explorer from "../src/Explorer";
+import { seedTransactions, clearSeeds } from "../script/seedTransactions";
+
+// Mock web3 and replace with ganache
+jest.mock("../src/web3Provider", () => {
+  function provider() {
+    return require("ganache-cli").provider();
+  }
+  return { provider };
+});
+
+let addresses = [];
+let alice = "";
+let bob = "";
+
+beforeAll(async () => {
+  // Alice -> Bob 5 ETH
+  addresses = await seedTransactions();
+  alice = addresses[0];
+  bob = addresses[1];
+});
+
+afterAll(() => {
+  clearSeeds();
+});
+
+describe("Explorer", () => {
+  describe("run()", () => {
+    describe("given only a start value", () => {
+      test("it searches the correct number of blocks", async () => {});
+    });
+
+    describe("given a range of blocks", () => {
+      test("it searches the correct number of blocks", async () => {});
+    });
+
+    describe("processes transaction data", () => {
+      test("it returns a hash of Addresses", async () => {
+        let explorer = new Explorer(0, 1);
+        expect(await explorer.run()).toEqual([0, 1]);
+        console.log("alice", alice);
+        console.log("bob", bob);
+      });
+    });
+    //test("it throws an error given a number > the current block number")
+
+    //test("it throws an error given a number < 0")
+
+    //
+  });
+});
